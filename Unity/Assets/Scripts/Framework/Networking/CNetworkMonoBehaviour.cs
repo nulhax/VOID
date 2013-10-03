91ce640701866657c945dc88b0d7c61823f48655
Bryce <bullfrogcs@hotmail.com>
2013-10-03 16:29:49 +1300

    fefe

---
+++ Unity/Assets/Scripts/Framework/Networking/CNetworkMonoBehaviour.cs
@@ -0,0 +1,68 @@
+﻿//  Auckland
+//  New Zealand
+//
+//  (c) 2013 VOID
+//
+//  File Name   :   CNetworkMonoBehaviour.cs
+//  Description :   --------------------------
+//
+//  Author  	:  Programming Team
+//  Mail    	:  contanct@spaceintransit.co.nz
+//
+
+
+// Namespaces
+using UnityEngine;
+using System.Collections;
+
+
+/* Implementation */
+
+
+public abstract class CNetworkMonoBehaviour : MonoBehaviour
+{
+
+// Member Types
+
+
+// Member Functions
+
+    // public:
+
+
+    public abstract void InitialiseNetworkVars();
+
+
+    public void InvokeRpc(uint _uiPlayerId, string _sMethodName, params object[] _caParameters)
+    {
+        NetworkView.InvokeRpc(_uiPlayerId, this, _sMethodName, _caParameters);
+    }
+
+
+    public void InvokeRpcAll(string _sMethodName, params object[] _caParameters)
+    {
+        NetworkView.InvokeRpcAll(this, _sMethodName, _caParameters);
+    }
+
+
+    public CNetworkView NetworkView
+    {
+        get { return (gameObject.GetComponent<CNetworkView>()); }
+    }
+
+
+    // protected:
+
+
+    // private:
+
+
+// Member Variables
+
+    // protected:
+
+
+    // private:
+
+
+};
