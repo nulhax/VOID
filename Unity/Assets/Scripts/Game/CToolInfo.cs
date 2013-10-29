eba4d7866cf2185ba481acfcf5d7c145e4847814
Matthew.Buist <rgbmml@hotmail.com>
2013-10-30 10:17:50 +1300

    Made 3 files, mostly blank, containing Template

---
+++ Unity/Assets/Scripts/Game/CToolInfo.cs
@@ -0,0 +1,56 @@
+//  Auckland
+//  New Zealand
+//
+//  (c) 2013
+//
+//  File Name   :   CToolInfo.cs
+//  Description :   --------------------------
+//
+//  Author  	:  
+//  Mail    	:  @hotmail.com
+//
+
+
+// Namespaces
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+
+/* Implementation */
+
+
+public class CToolInfo : MonoBehaviour
+{
+
+// Member Types
+
+
+// Member Delegates & Events
+
+
+// Member Properties
+
+
+// Member Functions
+
+
+	public void Start()
+	{
+	}
+
+
+	public void OnDestroy()
+	{
+	}
+
+
+	public void Update()
+	{
+	}
+
+
+// Member Fields
+
+
+};
