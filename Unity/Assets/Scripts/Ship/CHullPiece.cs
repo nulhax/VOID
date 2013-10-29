67d420a5aa24af7f52e75211cd3dfde239d879a7
Vindicer <Nathan.Boon@gmail.com>
2013-10-30 10:57:08 +1300

    Added base scripts for ship and room classes.

---
+++ Unity/Assets/Scripts/Ship/CHullPiece.cs
@@ -0,0 +1,56 @@
+﻿//  Auckland
+//  New Zealand
+//
+//  (c) 2013
+//
+//  File Name   :   CHullPiece.cs
+//  Description :   --------------------------
+//
+//  Author  	:  
+//  Mail    	:  @hotmail.com
+//
+
+
+// Namespaces
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+
+/* Implementation */
+
+
+public class CHullPiece : MonoBehaviour
+{
+
+// Member Types
+
+
+// Member Delegates & Events
+
+
+// Member Properties
+
+
+// Member Functions
+
+
+	public void Start()
+	{
+	}
+
+
+	public void OnDestroy()
+	{
+	}
+
+
+	public void Update()
+	{
+	}
+
+
+// Member Fields
+
+
+};
