0a011bb604f53229f4814d45f0d306d50d56132c
Vindicer <Nathan.Boon@gmail.com>
2013-11-04 13:10:06 +1300

    Added a few base scripts, and initial implementation of CRoomInfo. CRoomInfo is currently networked and untested.

--- Unity/Assets/Scripts/Ship/Rooms/CRoomFactory.cs
+++ Unity/Assets/Scripts/Ship/Rooms/CRoomFactory.cs
@@ -4,53 +4,59 @@
 //  (c) 2013
 //
 //  File Name   :   CRoomFactory.cs
-//  Description :   --------------------------
+//  Description :   Class script for the factory facility
 //
-//  Author  	:  
-//  Mail    	:  @hotmail.com
+//  Author  	:  Nathan Boon
+//  Mail    	:  Nathan.Boon@gmail.com
 //
-
 
 // Namespaces
 using UnityEngine;
+using System.IO;
+using System.Xml;
 using System.Collections;
 using System.Collections.Generic;
 
-
 /* Implementation */
-
-
 public class CRoomFactory : MonoBehaviour
 {
 
 // Member Types
-
+	struct TData
+	{
+		// XML Imports
+		float m_fHealth;
+		float m_fPowerCost;
+		float m_fRecharge;
+		float m_fRadiationLevel;
+		float m_fRadiationRadius;
+		
+		// Class
+		ushort m_sCurrentToolID;
+	};
 
 // Member Delegates & Events
 
-
 // Member Properties
 
-
 // Member Functions
-
-
 	public void Start()
 	{
+        // Load the XML reader and document for parsing information
+        TextAsset ImportedXml = new TextAsset();
+        ImportedXml.name = Utility.GetXmlPathFacilities();
+
+        XmlTextReader XReader = new XmlTextReader(new StringReader(ImportedXml.text));
+        XmlDocument XDoc = new XmlDocument();
+        XDoc.Load(XReader);
+
+        string T = XDoc.Attributes.ToString();
+        Debug.Break();
 	}
 
-
-	public void OnDestroy()
-	{
-	}
-
-
-	public void Update()
-	{
-	}
-
+	public void OnDestroy() {}
+	public void Update() {}
 
 // Member Fields
-
 
 };
---
+++ Unity/Assets/Scripts/Utility/Utility.cs
@@ -0,0 +1,55 @@
+﻿//  Auckland
+//  New Zealand
+//
+//  (c) 2013
+//
+//  File Name   :   Utility.cs
+//  Description :   Utility class containing helper functions
+//                  and plain text resource locations
+//
+//  Author  	:  Nathan Boon
+//  Mail    	:  Nathan.Boon@gmail.com
+//
+
+// Namespaces
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+/* Implementation */
+public class Utility
+{
+    // Member Types
+    static readonly string s_sXmlPath           = "../../Resources/XMLs/";
+    static readonly string s_sXmlPathTools      = s_sXmlPath + "Tools.xml";
+    static readonly string s_sXmlPathComponents = s_sXmlPath + "Components.xml";
+    static readonly string s_sXmlPathFacilities = s_sXmlPath + "Facilities.xml";
+
+    enum ETool
+    {
+        TOOL_INVALID = -1,
+        TOOL_BLOWTORCH,
+        TOOL_DETX,
+        TOOL_EXTINGUISHER,
+        TOOL_MEDPACK,
+        TOOL_NANITEGUN,
+        TOOL_PISTOL,
+        TOOL_SEALER,
+        TOOL_TECHKIT,
+        TOOL_TORCH,
+        TOOL_MAX
+    };
+
+    // Member Delegates & Events
+
+    // Member Properties
+
+    // Member Functions
+    static public string GetXmlPath()           { return (s_sXmlPath);           }
+    static public string GetXmlPathTools()      { return (s_sXmlPathTools);      }
+    static public string GetXmlPathComponents() { return (s_sXmlPathComponents); }
+    static public string GetXmlPathFacilities() { return (s_sXmlPathFacilities); }
+
+    // Member Fields
+
+};
