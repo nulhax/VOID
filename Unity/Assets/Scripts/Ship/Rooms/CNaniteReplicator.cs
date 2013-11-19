d2f9f0e76c5a24677c05d87cd10f7ef69ef2a77f
Scott Emery <scott.ipod@gmail.com>
2013-11-20 10:15:49 +1300

    Power generator stuff done here. Moving on to replicator
    Signed-off-by: Scott Emery <scott.ipod@gmail.com>

---
+++ Unity/Assets/Scripts/Ship/Rooms/CNaniteReplicator.cs
@@ -0,0 +1,69 @@
+﻿//  Auckland
+//  New Zealand
+//
+//  (c) 2013
+//
+//  File Name   :   CNaniteReplicator.cs
+//  Description :   --------------------------
+//
+//  Author  	:  Scott Emery
+//  Mail    	:  scott.ipod@gmail.com
+//
+
+
+// Namespaces
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+/* Implementation */
+
+public class CNaniteReplicator : CNetworkMonoBehaviour 
+{
+	
+// Member Types
+
+
+// Member Delegates & Events
+
+
+// Member Properties
+
+
+// Member Functions
+
+	public override void InstanceNetworkVars()
+	{
+
+	}
+	
+	// Use this for initialization
+	void Start () 
+	{
+	
+	}
+	
+
+	public void OnDestroy()
+	{
+		
+	}
+
+
+	// Update is called once per frame
+	void Update () 
+	{
+		// Get the size of the asteroid input into the replicator	
+		// Add the unit size to the amount of nanites in the replicator
+	
+	}
+	
+	
+	void OnNetworkVarSync(INetworkVar _cVarInstance)
+	{
+		
+	}
+	// Member Fields
+	float m_fAsteroidSize;
+	float m_fTotalNanites;
+}
