3ef11537f659715660ee03271110be0072925370
Martin Ponichtera <martin.nul@outlook.com>
2013-11-26 17:02:33 +1300

    Version 1 of the shield. Ellipsoid around the ship...

--- Unity/Assets/Scripts/Ship/CGalaxyShipCollider.cs
+++ Unity/Assets/Scripts/Ship/CGalaxyShipCollider.cs
@@ -38,5 +38,42 @@ public class CGalaxyShipCollider : MonoBehaviour
 		newCollider.transform.parent = m_CompoundCollider.transform;
 		newCollider.transform.localPosition = _RelativePos;
 		newCollider.transform.localRotation = _RelativeRot;
+		
+		// Move the collider to identity transform
+		Vector3 oldPos = m_CompoundCollider.transform.position;
+		Quaternion oldRot = m_CompoundCollider.transform.rotation;
+		m_CompoundCollider.transform.position = Vector3.zero;
+		m_CompoundCollider.transform.rotation = Quaternion.identity;
+		
+		// Update the shield bounds
+		Collider[] childColliders = m_CompoundCollider.GetComponentsInChildren<Collider>();
+        Bounds shieldBounds = new Bounds(Vector3.zero, Vector3.zero);
+        foreach(Collider collider in childColliders)
+        {
+            shieldBounds.Encapsulate(collider.bounds);
+        }
+		
+		// Update the shield bounds
+		gameObject.GetComponent<CGalaxyShipShield>().UpdateShieldBounds(shieldBounds);
+		
+		// Move back to old transform
+		m_CompoundCollider.transform.position = oldPos;
+		m_CompoundCollider.transform.rotation = oldRot;
+	}
+	
+	void Update()
+	{
+		if(Input.GetKeyDown(KeyCode.F10))
+		{
+			// Update the shield bounds
+			Renderer[] meshFilters = GetComponentsInChildren<Renderer>();
+	        Bounds shieldBounds = new Bounds(transform.position, Vector3.zero);
+	        foreach(Renderer meshFilter in meshFilters)
+	        {
+	            shieldBounds.Encapsulate(meshFilter.bounds);
+	        }
+			
+			gameObject.GetComponent<CGalaxyShipShield>().UpdateShieldBounds(shieldBounds);
+		}
 	}
 }
---
+++ Unity/Assets/Scripts/Ship/CGalaxyShipShield.cs
@@ -0,0 +1,61 @@
+﻿//  Auckland
+//  New Zealand
+//
+//  (c) 2013 VOID
+//
+//  File Name   :   CGalaxyShipShield.cs
+//  Description :   --------------------------
+//
+//  Author      :  Programming Team
+//  Mail        :  contanct@spaceintransit.co.nz
+//
+
+
+// Namespaces
+using UnityEngine;
+using System.Collections;
+
+
+/* Implementation */
+
+
+public class CGalaxyShipShield : MonoBehaviour 
+{
+	// Member Types
+	public enum EShieldState
+	{
+		INVALID,
+		
+		PoweredUp,
+		PoweredDown,
+		Reacting,
+		Charging,
+		
+		MAX
+	}
+	
+	// Member Fields
+	public GameObject m_Shield = null;
+	
+	private float m_ShieldPower;
+	private float m_MaxShieldPower;
+	
+	private bool m_Active = true;
+	private EShieldState m_ShieldState = EShieldState.PoweredDown;
+	
+	// Member Properies
+	
+	
+	// Member Methods
+	public void UpdateShieldBounds(Bounds _ShieldBounds)
+	{	
+		Vector3 scale = Vector3.zero;
+		scale.x = _ShieldBounds.size.x / Mathf.Sqrt(2.0f) * 2.0f;
+		scale.z = _ShieldBounds.size.z / Mathf.Sqrt(2.0f) * 2.0f;
+		scale.y = _ShieldBounds.size.y * 2.0f;
+		
+		// Set as the scale of the object and increase the scale a little
+		m_Shield.transform.localScale = scale * 1.1f;
+		m_Shield.transform.localPosition = _ShieldBounds.center;
+	}
+}
