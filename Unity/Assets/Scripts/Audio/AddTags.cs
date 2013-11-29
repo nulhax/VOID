326fec046050000ada2ad2328a668c5f93150bc2
Daniel <folduppugg@hotmail.com>
2013-11-29 15:06:11 +1300

    Added all the missing audio scripts.

---
+++ Unity/Assets/Scripts/Audio/AddTags.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AddTags : MonoBehaviour 
+{
+
+	// Use this for initialization
+	void Start () 
+	{
+		Transform[] children = gameObject.GetComponentsInChildren<Transform>();
+		
+		foreach(Transform child in children)
+		{
+			child.tag = "Listener";
+		}		
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+	
+	}
+}
