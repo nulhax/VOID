344b7b3a684dd76602066e9ce2d29eb205a8c4a9
Daniel Langsford <folduppugg@hotmail.com>
2013-12-03 16:10:45 +1300

    Changed nothing, actually.

--- Unity/Assets/Scripts/Audio/AddTags.cs
+++ Unity/Assets/Scripts/Audio/AddTags.cs
@@ -7,6 +7,7 @@ public class AddTags : MonoBehaviour
 	// Use this for initialization
 	void Start () 
 	{
+		int i =0;
 		Transform[] children = gameObject.GetComponentsInChildren<Transform>();
 		
 		foreach(Transform child in children)
