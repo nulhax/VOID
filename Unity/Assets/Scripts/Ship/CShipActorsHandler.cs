e02d2342fa33bba9af3ba22701a1d57e0d09b061
Martin Ponichtera <martin.nul@outlook.com>
2013-12-03 13:24:10 +1300

    Added the internal trigger bounds for the bridge and the hallway. Gravity currently affects things within these bounding volumes - ready for atmosphere to do the same. Moved and renamed a few scripts. Some of them don't make sense as hallways are treated as facilities atm... but this will need to change im guessing.

---
+++ Unity/Assets/Scripts/Ship/CShipActorsHandler.cs
@@ -0,0 +1,61 @@
+﻿//  Auckland
+//  New Zealand
+//
+//  (c) 2013
+//
+//  File Name   :   CShipInhabitants.cs
+//  Description :   --------------------------
+//
+//  Author  	:  
+//  Mail    	:  @hotmail.com
+//
+
+
+// Namespaces
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using System;
+
+
+/* Implementation */
+
+
+public class CShipActorsHandler : MonoBehaviour 
+{
+	// Member Types
+
+
+	// Member Delegates & Events
+
+
+	// Member Fields
+	private GameObject m_ShipActorsContainer = null;
+	private List<GameObject> m_ActorsCurrentlyOnboard = new List<GameObject>();
+	
+	// Member Properties
+	
+	
+	// Member Methods
+	public void RegisterActorEnterExit(Action<GameObject> _EnterHandlerDelegate, Action<GameObject> _ExitHandlerDelegate)
+	{
+		_EnterHandlerDelegate += ActorEnteredFacility;
+		_ExitHandlerDelegate += ActorExitedFacility;
+	}
+	
+	private void ActorEnteredFacility(GameObject _Actor)
+	{
+		if(!m_ActorsCurrentlyOnboard.Contains(_Actor))
+		{
+			m_ActorsCurrentlyOnboard.Add(_Actor);
+		}
+	}
+	
+	private void ActorExitedFacility(GameObject _Actor)
+	{
+		if(m_ActorsCurrentlyOnboard.Contains(_Actor))
+		{
+			m_ActorsCurrentlyOnboard.Remove(_Actor);
+		}
+	}
+}
---
+++ Unity/Assets/Scripts/Ship/Facilities/CFacilityGravity.cs
@@ -0,0 +1,80 @@
+﻿//  Auckland
+//  New Zealand
+//
+//  (c) 2013
+//
+//  File Name   :   CFacilityGravity.cs
+//  Description :   --------------------------
+//
+//  Author  	:  
+//  Mail    	:  @hotmail.com
+//
+
+
+// Namespaces
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using System;
+
+
+/* Implementation */
+
+
+public class CFacilityGravity : MonoBehaviour 
+{
+	// Member Types
+
+
+	// Member Delegates & Events
+
+	
+	// Member Fields
+	private List<GameObject> m_ActorsInsideTrigger = new List<GameObject>();
+	private Vector3 m_FacilityGravityAcceleration = new Vector3(0.0f, -9.81f, 0.0f);
+	
+	// Member Properties
+	
+	
+	// Member Methods
+	public void Awake()
+	{
+		// Register the actors entering/exiting the trigger zone
+		CInteriorTrigger facilityInteriorTrigger = GetComponentInChildren<CInteriorTrigger>();
+		
+		if(facilityInteriorTrigger == null)
+			Debug.LogError("CFacilityGravity, no interior trigger to use for gravity application!");
+		
+		facilityInteriorTrigger.ActorEnteredTrigger += ActorEnteredGravityZone;
+		facilityInteriorTrigger.ActorExitedTrigger += ActorExitedGravityZone;
+	}
+	
+	
+	public void Update()
+	{
+		// Apply the gravity to the actor every frame (so we can modify it if we want later)
+		foreach(GameObject actor in m_ActorsInsideTrigger)
+		{
+			actor.GetComponent<CDynamicActor>().GravityAcceleration = m_FacilityGravityAcceleration;
+		}
+	}
+	
+	private void ActorEnteredGravityZone(GameObject _Actor)
+	{
+		// Only add to the list if there is a dynamic actor
+		if(_Actor.GetComponent<CDynamicActor>() == null)
+			return;
+		
+		m_ActorsInsideTrigger.Add(_Actor);
+	}
+	
+	private void ActorExitedGravityZone(GameObject _Actor)
+	{
+		if(!m_ActorsInsideTrigger.Contains(_Actor))
+			return;
+		
+		_Actor.GetComponent<CDynamicActor>().GravityAcceleration = Vector3.zero;
+			
+		m_ActorsInsideTrigger.Remove(_Actor);
+	}
+}
