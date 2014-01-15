7e168b2738f4ce1cbf0bb3ea66f2224951e739d3
Jade Abbott <20chimps@gmail.com>
2014-01-16 00:07:21 +1300

    Added CNetworkView to Galaxy Projector prefab, though that still does not fix the issue.

--- Unity/Assets/Scripts/Galaxy/GalaxyProjector.cs
+++ Unity/Assets/Scripts/Galaxy/GalaxyProjector.cs
@@ -1,6 +1,7 @@
 ﻿using UnityEngine;
 using System.Collections;
 
+[RequireComponent(typeof(CNetworkView))]
 [RequireComponent(typeof(ParticleEmitter))]
 public class GalaxyProjector : CNetworkMonoBehaviour
 {
