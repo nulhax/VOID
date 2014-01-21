d680a52fb05fbf96ec19aaae28097cada73254e2
Jade Abbott <20chimps@gmail.com>
2014-01-22 10:45:38 +1300

    Added enemy ship stuff - not finished, but it does not conflict with anything.

---
+++ Unity/Assets/Scripts/Enemy Ship/CEnemyShip.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CEnemyShip : MonoBehaviour
+{
+	void Start ()
+    {
+        transform.rotation = Random.rotation;
+	}
+	
+	void Update ()
+    {
+        GameObject playerShip = CGameShips.GalaxyShip;
+        if (playerShip)
+        {
+            Vector3 dirToPlayerShip = (playerShip.transform.position - gameObject.transform.position).normalized;
+            gameObject.rigidbody.AddTorque(Quaternion.Angle(gameObject.transform.rotation, Quaternion.LookRotation(dirToPlayerShip)));
+            gameObject.rigidbody.AddForce(dirToPlayerShip * 10.0f, ForceMode.VelocityChange);
+        }
+	}
+}
