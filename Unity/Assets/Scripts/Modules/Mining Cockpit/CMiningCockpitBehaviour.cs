4abdb08cf2238e46a32a42cefccb1c6eb07d90f4
Bryce <bullfrogcs@hotmail.com>
2014-01-22 14:18:38 +1300

    Forgot to add files

---
+++ Unity/Assets/Scripts/Modules/Mining Cockpit/CMiningCockpitBehaviour.cs
@@ -0,0 +1,67 @@
+//  Auckland
+//  New Zealand
+//
+//  (c) 2013
+//
+//  File Name   :   CLASSNAME.cs
+//  Description :   --------------------------
+//
+//  Author  	:  
+//  Mail    	:  @hotmail.com
+//
+
+
+// Namespaces
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+
+/* Implementation */
+
+
+public class CMiningCockpitBehaviour : CNetworkMonoBehaviour
+{
+
+// Member Types
+
+
+// Member Delegates & Events
+
+
+// Member Properties
+
+
+// Member Methods
+
+
+	public override void InstanceNetworkVars()
+	{
+		
+	}
+
+
+	void Start()
+	{
+	}
+
+
+	void OnDestroy()
+	{
+	}
+
+
+	void Update()
+	{
+	}
+
+
+	void OnNetworkVarSync(INetworkVar _cSyncedVar)
+	{
+	}
+
+
+// Member Fields
+
+
+};
