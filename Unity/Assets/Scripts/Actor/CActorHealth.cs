ac0faa647439c699e9aed8fa8b35a3006f7e3225
Martin Ponichtera <martin.nul@outlook.com>
2014-02-03 17:25:27 +1300

    Jade...

--- Unity/Assets/Scripts/Actor/CActorHealth.cs
+++ Unity/Assets/Scripts/Actor/CActorHealth.cs
@@ -48,8 +48,8 @@ public class CActorHealth : CNetworkMonoBehaviour
 
 	public override void InstanceNetworkVars(CNetworkViewRegistrar _cRegistrar)
 	{
-		health_internal = _cRegistrar.CreateNetworkVar<float>(OnSyncHealth, health_initial);
-		state_internal = _cRegistrar.CreateNetworkVar<byte>(OnSyncState, state_initial);
+		health_internal = _cRegistrar.CreateNetworkVar<float>(OnSyncHealth, float.MinValue);
+		state_internal = _cRegistrar.CreateNetworkVar<byte>(OnSyncState, byte.MinValue);
 		// Set before Start()
 		health_previous = health_current = health_initial;
 		state_previous = state_current = state_initial;
@@ -57,7 +57,8 @@ public class CActorHealth : CNetworkMonoBehaviour
 
 	void Start()
 	{
-
+		if(CNetwork.IsServer)
+			UpdateHealthAndStates();
 	}
 
 	void Update()
@@ -67,15 +68,20 @@ public class CActorHealth : CNetworkMonoBehaviour
 			timeUntilNextNetworkSync -= Time.deltaTime;
 			if (timeUntilNextNetworkSync <= 0.0f && (syncNetworkHealth || syncNetworkState))
 			{
-				if (syncNetworkHealth && health_current != health_internal.Get())
-					health_internal.Set(health_current);
-
-				if (syncNetworkState && state_current != state_internal.Get())
-					state_internal.Set(state_current);
+				UpdateHealthAndStates();
 
 				timeUntilNextNetworkSync = timeBetweenNetworkSyncs;
 			}
 		}
+	}
+
+	void UpdateHealthAndStates()
+	{
+		if (syncNetworkHealth && health_current != health_internal.Get())
+			health_internal.Set(health_current);
+		
+		if (syncNetworkState && state_current != state_internal.Get())
+			state_internal.Set(state_current);
 	}
 
 	void OnSyncHealth(INetworkVar sender)
