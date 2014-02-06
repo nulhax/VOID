b2995e0b6056b2190cdba04c40d11eb55ed2adba
Jade Abbott <20chimps@gmail.com>
2014-02-07 12:04:24 +1300

    Made all hull breaches inform hull of a breach.

--- Unity/Assets/Scripts/Hazards/Hull Breach/CHullBreachNode.cs
+++ Unity/Assets/Scripts/Hazards/Hull Breach/CHullBreachNode.cs
@@ -8,6 +8,8 @@ public class CHullBreachNode : MonoBehaviour
 	public Mesh goodMesh = null;
 	public Mesh breachedMesh = null;
 
+	private CFacilityHull parentFacilityHull = null;
+
 	private System.Collections.Generic.List<CHullBreachNode> childBreaches = new System.Collections.Generic.List<CHullBreachNode>();
 	uint numChildrenBreached = 0;
 
@@ -19,6 +21,14 @@ public class CHullBreachNode : MonoBehaviour
 	{
 		GetComponent<CActorHealth>().EventOnSetState += OnSetState;
 
+		Transform parentTransform = gameObject.transform.parent;
+		if (parentTransform != null)
+		{
+			CFacilityHull parentHull = parentTransform.GetComponent<CFacilityHull>();
+			if (parentHull)
+				SetFacilityHull(parentHull);
+		}
+
 		foreach (Transform child in transform)
 		{
 			CHullBreachNode childBreachNode = child.GetComponent<CHullBreachNode>();
@@ -27,6 +37,18 @@ public class CHullBreachNode : MonoBehaviour
 				childBreachNode.EventOnSetBreached += OnChildSetBreached;
 				childBreaches.Add(childBreachNode);
 			}
+		}
+	}
+
+	private void SetFacilityHull(CFacilityHull facilityHull)
+	{
+		parentFacilityHull = facilityHull;
+
+		foreach (Transform child in transform)
+		{
+			CHullBreachNode childBreachNode = child.GetComponent<CHullBreachNode>();
+			if (childBreachNode != null)
+				childBreachNode.SetFacilityHull(facilityHull);
 		}
 	}
 
@@ -76,13 +98,8 @@ public class CHullBreachNode : MonoBehaviour
 						hullBreachNode.breached = true;
 
 						// Inform the facility this breach resides in.
-						Transform parentTransform = gameObject.transform.parent;
-						if (parentTransform != null)
-						{
-							CFacilityHull parentHull = parentTransform.GetComponent<CFacilityHull>();
-							if (parentHull)
-								parentHull.AddBreach(gameObject);
-						}
+						if (hullBreachNode.parentFacilityHull != null)
+							hullBreachNode.parentFacilityHull.AddBreach(gameObject);
 					}
 				}
 				break;
@@ -112,13 +129,8 @@ public class CHullBreachNode : MonoBehaviour
 						hullBreachNode.breached = false;
 
 						// Inform the facility this breach resides in.
-						Transform parentTransform = gameObject.transform.parent;
-						if (parentTransform != null)
-						{
-							CFacilityHull parentHull = parentTransform.GetComponent<CFacilityHull>();
-							if (parentHull)
-								parentHull.RemoveBreach(gameObject);
-						}
+						if (hullBreachNode.parentFacilityHull != null)
+							hullBreachNode.parentFacilityHull.RemoveBreach(gameObject);
 					}
 				}
 				break;
