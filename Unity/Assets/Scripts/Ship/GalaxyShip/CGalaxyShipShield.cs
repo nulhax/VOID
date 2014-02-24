a0d942ef1dd437d3c3de8e91c0a368a53363ca0e
Scott Emery <scott.ipod@gmail.com>
2014-02-25 10:34:35 +1300

    asdgfasdfga
    Signed-off-by: Scott Emery <scott.ipod@gmail.com>

--- Unity/Assets/Scripts/Ship/GalaxyShip/CGalaxyShipShield.cs
+++ Unity/Assets/Scripts/Ship/GalaxyShip/CGalaxyShipShield.cs
@@ -18,7 +18,6 @@ using System.Collections;
 
 /* Implementation */
 
-
 public class CGalaxyShipShield : CNetworkMonoBehaviour 
 {
 	// Member Types
@@ -39,34 +38,44 @@ public class CGalaxyShipShield : CNetworkMonoBehaviour
 	
 	private float m_ShieldPower = 100.0f;
 	private const float m_MaxShieldPower = 100.0f;
-
+	
 	// Take this from the module to determine active
 	private bool m_Active = true;
-
+	
 	private EShieldState m_ShieldState = EShieldState.PoweredDown;
-	 
+	
 	private const float cShipRechargeRate = 1.0f;
 	
 	// Member Properies
-
+	
+	public bool bActive
+	{
+		// Get if the shield is active, don't allow others to set
+		get { return(m_Active); }
+	}
+	
+	public float fShieldPower
+	{
+		// Get the shield power, don't allow others to set
+		get { return(m_ShieldPower); }
+	}
+	
 	// Member Methods
-
-
 	void Start()
 	{
-//		gameObject.GetComponentInChildren<CShieldEventHandler>().EventShieldCollider += ShipShieldCollider;
-//		gameObject.GetComponentInChildren<CShieldEventHandler>().EventShieldDamage += ShipShieldDamage;
-//		gameObject.GetComponentInChildren<CShieldEventHandler>().EventShieldRecharge += ShieldRecharge;
+		gameObject.GetComponentInChildren<CShieldEventHandler>().EventShieldCollider += ShipShieldCollider;
+		gameObject.GetComponentInChildren<CShieldEventHandler>().EventShieldDamage += ShipShieldDamage;
+		gameObject.GetComponentInChildren<CShieldEventHandler>().EventShieldRecharge += ShieldRecharge;
 	}
-
+	
 	public void UpdateShieldBounds(Mesh _ShieldMesh)
 	{	
 		m_Shield.GetComponent<MeshFilter>().mesh = _ShieldMesh;
-
+		
 		m_Shield.GetComponent<MeshCollider>().sharedMesh = null;
 		m_Shield.GetComponent<MeshCollider>().sharedMesh = _ShieldMesh;
 	}
-
+	
 	void ShipShieldCollider(Collider _Collider)
 	{
 		if(_Collider.gameObject.tag == "Asteroid")
@@ -74,16 +83,16 @@ public class CGalaxyShipShield : CNetworkMonoBehaviour
 			if(_Collider.gameObject.tag == "Asteroid")
 			{
 				Debug.Log("Collider entered Trigger as asteroid");
-
+				
 				// Take the ship velocity and add it to the asteroid velocity
 				//TODO: Make this betterer
 				Vector3 Move = CGameShips.GalaxyShip.rigidbody.velocity;
-
+				
 				_Collider.gameObject.transform.rigidbody.velocity = Move;
 			}
 		}
 	}
-
+	
 	void ShipShieldDamage(Collider _Collider)
 	{
 		if(CNetwork.IsServer)
@@ -93,19 +102,25 @@ public class CGalaxyShipShield : CNetworkMonoBehaviour
 			{
 				// Set health to zero
 				m_ShieldPower = 0.0f;
-
+				
+				// Set the network var
+				m_fVarShieldPower.Set(m_ShieldPower);
+				
 				// Let shield activity be false
 				m_Active = false;
+				
+				// Set the network var
+				m_bVarIsActive.Set (m_Active);
 			}
 			else
 			{
 				m_ShieldPower -= fDamage;
 			}
-
+			
 			Debug.Log ("Shield Health is at: " + m_ShieldPower);
 		}
 	}
-
+	
 	void ShieldRecharge()
 	{
 		if(CNetwork.IsServer)
@@ -116,7 +131,7 @@ public class CGalaxyShipShield : CNetworkMonoBehaviour
 				if(m_ShieldPower < m_MaxShieldPower)
 				{
 					m_ShieldPower += cShipRechargeRate * Time.deltaTime;
-
+					
 					Debug.Log ("Shield Health: " + m_ShieldPower);
 				}
 			}
@@ -127,10 +142,12 @@ public class CGalaxyShipShield : CNetworkMonoBehaviour
 			}
 		}
 	}
-
+	
 	void OnNetworkVarSync(INetworkVar _cSyncedNetworkVar)
 	{
+		m_Active = m_bVarIsActive.Get();
 		
+		m_ShieldPower = m_fVarShieldPower.Get();
 	}
 	
 	
@@ -138,9 +155,7 @@ public class CGalaxyShipShield : CNetworkMonoBehaviour
 	{
 		
 	}
-	void OnGui()
-	{
-
-	}
-
+	
+	CNetworkVar<bool> m_bVarIsActive;
+	CNetworkVar<float> m_fVarShieldPower;
 }
