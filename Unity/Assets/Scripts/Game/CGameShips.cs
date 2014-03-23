13ea5beeac6ec4b72d32e6b29dfa28ced562d075
Bryce <bullfrogcs@hotmail.com>
2014-03-23 18:45:58 +1300

    Fixedup airlock facility. Changed player actor layer to default

--- Unity/Assets/Scripts/Game/CGameShips.cs
+++ Unity/Assets/Scripts/Game/CGameShips.cs
@@ -111,10 +111,10 @@ public class CGameShips : CNetworkMonoBehaviour
 		m_cShipViewId = cShipObject.GetComponent<CNetworkView>().ViewId;
 
         GameObject cBirdgeObject = CNetwork.Factory.CreateObject(CFacilityInterface.GetPrefabType(CFacilityInterface.EType.Bridge));
-       // cBirdgeObject.GetComponent<CFacilityExpansion>().GetExpansionPort(0).GetComponent<CExpansionPortBehaviour>().CreateFacility(CFacilityInterface.EType.Airlock, 0);
+        cBirdgeObject.GetComponent<CFacilityExpansion>().GetExpansionPort(0).GetComponent<CExpansionPortBehaviour>().CreateFacility(CFacilityInterface.EType.Airlock, 0);
 
 
-        cBirdgeObject.GetComponent<CFacilityExpansion>().GetExpansionPort(0).GetComponent<CExpansionPortBehaviour>().CreateFacility(CFacilityInterface.EType.Test, 0);
+        //cBirdgeObject.GetComponent<CFacilityExpansion>().GetExpansionPort(0).GetComponent<CExpansionPortBehaviour>().CreateFacility(CFacilityInterface.EType.Test, 0);
 
 
 		//cShipObject.GetComponent<CShipFacilities>().CreateFacility(CFacilityInterface.EType.Bridge);
