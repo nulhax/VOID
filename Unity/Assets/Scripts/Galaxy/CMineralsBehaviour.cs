06c5c276d7552671e22d451efc32769d06b6aea0
Bryce <bullfrogcs@hotmail.com>
2014-03-23 16:23:19 +1300

    Adding new facility pieces and NGUI textures

---
+++ Unity/Assets/Scripts/Galaxy/CMineralsBehaviour.cs
@@ -0,0 +1,121 @@
+//  Auckland
+//  New Zealand
+//
+//  (c) 2013
+//
+//  File Name   :   CMinerals.cs
+//  Description :   --------------------------
+//
+//  Author  	:  
+//  Mail    	:  @hotmail.com
+//
+
+
+// Namespaces
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+
+/* Implementation */
+
+
+public class CMineralsBehaviour : CNetworkMonoBehaviour
+{
+
+// Member Types
+
+
+// Member Delegates & Events
+
+
+    public delegate void HandleDepleted(GameObject _cMineral);
+    public event HandleDepleted EventDeplete;
+
+
+// Member Properties
+
+
+	public float Quantity
+	{ 
+		set { m_fQuantity.Set(value); }
+		
+		get { return m_fQuantity.Get(); } 
+	}
+
+
+    public bool IsDepleted
+    {
+        get { return (m_bDepleted); }
+    }
+
+
+// Member Methods
+
+
+	public override void InstanceNetworkVars(CNetworkViewRegistrar _cRegistrar)
+	{
+		m_fQuantity = _cRegistrar.CreateNetworkVar<float>(OnNetworkVarSync, 300.0f);
+	}
+
+
+	public float DecrementQuanity(float _fQuantity)
+	{
+        if (!IsDepleted)
+        {
+            if (_fQuantity <= m_fQuantity.Get())
+            {
+                m_fQuantity.Set(m_fQuantity.Get() - _fQuantity);
+            }
+            else
+            {
+                _fQuantity = m_fQuantity.Get();
+                m_fQuantity.Set(0.0f);
+                m_bDepleted = true;
+
+                if (EventDeplete != null) EventDeplete(gameObject);
+
+                CNetwork.Factory.DestoryObject(gameObject);
+            }
+
+            CGameShips.Ship.GetComponent<CShipNaniteSystem>().AddNanites(_fQuantity);
+        }
+
+        return (m_fQuantity.Get());
+	}
+
+
+	void Start()
+	{
+		if(CNetwork.IsServer)
+		{
+			//ResourceQuanity = CGalaxy.instance.CalculateAsteroidResourceAmount(CGalaxy.instance.RelativePointToAbsoluteCell(transform.position));
+		}
+	}
+
+
+	void Update()
+	{
+		// Empty
+	}
+
+
+	void OnNetworkVarSync(INetworkVar _cSyncedVar)
+	{
+		if (_cSyncedVar == m_fQuantity)
+		{
+			//gameObject.GetComponentInChildren<Renderer>.material.SetColor("_Color", new Color(1.0f - Quantity, 1.0f - Quantity, 1.0f));
+		}
+	}
+
+
+// Member Fields
+
+
+	CNetworkVar<float> m_fQuantity = null;
+
+
+    bool m_bDepleted = false;
+
+
+};
