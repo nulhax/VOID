7d352d78e67fdb3b4a4b576b77449f66dd87afbb
Nathan Boon <Nathan.Boon@gmail.com>
2014-04-01 13:45:52 +1300

    Committing to pull.

---
+++ Unity/Assets/Scripts/User Interface/HUD/CHUDScore.cs
@@ -0,0 +1,56 @@
+﻿//  Auckland
+//  New Zealand
+//
+//  (c) 2013
+//
+//  File Name   :   CHazardSystem.cs
+//  Description :   --------------------------
+//
+//  Author  	:  
+//  Mail    	:  @hotmail.com
+//
+
+
+// Namespaces
+using UnityEngine;
+using System.Collections;
+
+public class CHUDScore : MonoBehaviour
+{
+// Member Properties
+    public uint Score
+    {
+        // Get
+        get { return (m_uiScore); }
+    }
+
+// Member Functions
+	void Start()
+    {
+        foreach (GameObject Actor in CGamePlayers.PlayerActors)
+        {
+            Actor.GetComponent<CPlayerHealth>().m_EventHealthChanged += TESTDamage;
+        }
+	}
+
+    private void TESTDamage(GameObject _TargetPlayer, float _fHealthCurrentValue, float _fHealthPreviousValue)
+    {
+        m_uiScore += 10;
+
+        Debug.Log("Score: " + Score);
+    }
+
+    private void OnDestroy()
+    {
+        
+    }
+	
+	
+
+// Unused Functions
+    void Awake(){}
+    void Update(){}
+
+// Member Fields
+    private uint m_uiScore = 0;
+}
