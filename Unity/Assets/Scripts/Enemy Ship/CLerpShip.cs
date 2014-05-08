f18b533b944ed5f5941ad9e9959c6bc23ea11399
Scott Emery <scott.ipod@gmail.com>
2014-05-08 16:28:00 +1200

    Main menu almost finished
    Signed-off-by: Scott Emery <scott.ipod@gmail.com>

---
+++ Unity/Assets/Scripts/Enemy Ship/CLerpShip.cs
@@ -0,0 +1,71 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CLerpShip : MonoBehaviour {
+
+	public bool IsActive
+	{
+		get { return(m_bIsActive); }
+		set { m_bIsActive = value ;}
+	}
+	
+	public float fLerpDuration = 20.0f;
+	public float m_fLerpTimer = 0.0f;
+	Vector3 shipPos = new Vector3(0.0f, 0.0f, 0.0f);
+	Vector3 SpawnPos = new Vector3(0.0f, 0.0f, -500.0f);
+	Vector3 EndPos = new Vector3(0.0f, 0.0f, 10000.0f);
+
+	// Use this for initialization
+	void Start () 
+	{
+		ResetShip();
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		//Lerp
+		if(IsActive)
+		{		
+			m_fLerpTimer += Time.deltaTime;
+			
+			float fLerpFactor = m_fLerpTimer / fLerpDuration;
+			transform.position = Vector3.Lerp(SpawnPos, EndPos, fLerpFactor);
+			
+			if(m_fLerpTimer > fLerpDuration)
+			{
+				m_fLerpTimer = 0.0f;
+
+				IsActive = false;
+				
+				ResetShip();
+			}
+		}
+	}
+
+	public void BeginShipMove()
+	{
+		IsActive = true;
+		transform.position = SpawnPos;
+
+		Renderer[] renderers = GetComponentsInChildren<Renderer>();
+		foreach(Renderer renderer in renderers)
+		{
+			renderer.enabled = true;
+		}
+		
+	}
+	
+	public void ResetShip()
+	{
+		transform.position = SpawnPos;
+		IsActive = false;
+
+		Renderer[] renderers = GetComponentsInChildren<Renderer>();
+		foreach(Renderer renderer in renderers)
+		{
+			renderer.enabled = false;
+		}
+	}
+	private bool m_bIsActive = false;
+}
---
+++ Unity/Assets/Scripts/Galaxy/CMenuShipBehaviour.cs
@@ -0,0 +1,69 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CMenuShipBehaviour : MonoBehaviour {
+
+
+	public float SpawnFreq = 15.0f;
+	public float fSpawnTimer = 0.0f;
+
+
+
+	// Use this for initialization
+	void Start () 
+	{
+		// Load ships
+		m_ShipArray[0] = (GameObject)GameObject.Instantiate(Resources.Load("Prefabs/EnemyShips/EnemyShip2"));
+		m_ShipArray[1] = (GameObject)GameObject.Instantiate(Resources.Load("Prefabs/EnemyShips/EnemyShip3"));
+		m_ShipArray[2] = (GameObject)GameObject.Instantiate(Resources.Load("Prefabs/EnemyShips/EnemyShip8"));
+
+
+		// Set ship render to false and remove  unwanted scripts
+		foreach( GameObject Ship in  m_ShipArray)
+		{
+			Destroy(Ship.GetComponent<GalaxyShiftable>());
+			Destroy(Ship.GetComponent<CEnemyShip>());
+			Destroy(Ship.GetComponent<NetworkedEntity>());
+			Destroy(Ship.GetComponent<CActorHealth>());
+			Destroy(Ship.GetComponent<CNetworkView>());
+
+			MonoBehaviour[] behaviours = Ship.GetComponentsInChildren<MonoBehaviour>();
+			
+			foreach(MonoBehaviour behaviour in behaviours)
+			{
+				Destroy(behaviour);
+			}
+
+			Ship.collider.isTrigger = true;
+
+			Ship.AddComponent<CLerpShip>();
+		}
+
+		// Lerp variables
+		isLerpActive = false;
+		m_fLerpTimer = 0.0f;
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		fSpawnTimer += Time.deltaTime;
+
+		if(fSpawnTimer > SpawnFreq)
+		{
+			m_iCurrentShipIndex = Random.Range(0, 3);
+
+			TriggerDummyMovement();
+			fSpawnTimer = 0.0f;
+		}
+	}
+
+	void TriggerDummyMovement()
+	{
+		m_ShipArray[m_iCurrentShipIndex].GetComponent<CLerpShip>().BeginShipMove();
+		Debug.Log("Dummy " + m_iCurrentShipIndex.ToString() + " now active");
+	}
+
+	private GameObject[] m_ShipArray = new GameObject[3];
+	private int m_iCurrentShipIndex = 0;
+}
