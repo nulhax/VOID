1fd9357444d2c244ae959dde5bd20fff713bfbfe
Scott Emery <scott.ipod@gmail.com>
2014-05-09 14:46:45 +1200

    Working on main menu, window movements and selections
    Signed-off-by: Scott Emery <scott.ipod@gmail.com>

--- Unity/Assets/Scripts/Enemy Ship/CLerpShip.cs
+++ Unity/Assets/Scripts/Enemy Ship/CLerpShip.cs
@@ -12,18 +12,31 @@ public class CLerpShip : MonoBehaviour {
 	public float fLerpDuration = 20.0f;
 	public float m_fLerpTimer = 0.0f;
 	Vector3 shipPos = new Vector3(0.0f, 0.0f, 0.0f);
-	Vector3 SpawnPos = new Vector3(0.0f, 0.0f, -500.0f);
-	Vector3 EndPos = new Vector3(0.0f, 0.0f, 10000.0f);
+	Vector3 SpawnPos = new Vector3(0.0f, -100.0f, -500.0f);
+	Vector3 EndPos = new Vector3(0.0f, -100.0f, 10000.0f);
+
+
 
 	// Use this for initialization
 	void Start () 
 	{
 		ResetShip();
+		m_bIsRandX = true;
 	}
 	
 	// Update is called once per frame
 	void Update () 
 	{
+		// rand the x so the ships spawn differently each time...Maybe get some curve later
+		if(m_bIsRandX)
+		{
+			int RandX = Random.Range(-100, 100);
+			SpawnPos.x = RandX;
+			EndPos.x = RandX;
+
+			m_bIsRandX = false;
+		}
+
 		//Lerp
 		if(IsActive)
 		{		
@@ -60,6 +73,7 @@ public class CLerpShip : MonoBehaviour {
 	{
 		transform.position = SpawnPos;
 		IsActive = false;
+		m_bIsRandX = true;
 
 		Renderer[] renderers = GetComponentsInChildren<Renderer>();
 		foreach(Renderer renderer in renderers)
@@ -68,4 +82,5 @@ public class CLerpShip : MonoBehaviour {
 		}
 	}
 	private bool m_bIsActive = false;
+	private bool m_bIsRandX = false;
 }
