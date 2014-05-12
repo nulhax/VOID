a1b653608a6d69606214dbeef9845f5915b62e26
Scott Emery <scott.ipod@gmail.com>
2014-05-12 22:57:23 +1200

    Working on getting the menu working properly, will be sorting networking in the morning. Will probably need help with the ordering of stuff, it's looking pretty messy atm.
    Signed-off-by: Scott Emery <scott.ipod@gmail.com>

---
+++ Unity/Assets/Scripts/Menu/CServerMenu.cs
@@ -0,0 +1,57 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CServerMenu : MonoBehaviour 
+{
+	public void ServerName()
+	{
+		m_sServerTitle = UIInput.current.value;
+	}
+
+	public void Port()
+	{
+		m_strRemoteServerPort = UIInput.current.value;
+	}
+
+	public void PlayerName()
+	{
+		m_sPlayerName = UIInput.current.value;
+	}
+	
+
+	// Use this for initialization
+	void Start () 
+	{
+
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+
+	}
+
+	void OnClick()
+	{
+
+	}
+
+	string m_sServerTitle = System.Environment.UserDomainName + ": " + System.Environment.UserName;
+	string m_sPlayerName = "Enter name";
+	string[] m_saTabTitles = { "Online Servers", "Lan Servers" };
+	
+	
+	float m_fNumSlots = 16.0f;
+	
+	
+	int m_iActiveTab = 1;
+
+	// Manual server connection variables
+	string m_strRemoteServerIP = "127.0.0.1";
+	string m_strRemoteServerPort = "1337";
+	ushort m_usRemoteServerPort = 0;
+}
+
+// http://www.tasharen.com/forum/index.php?topic=1501.0
+// http://www.tasharen.com/forum/index.php?topic=6752.0
+// http://www.tasharen.com/?page_id=693
