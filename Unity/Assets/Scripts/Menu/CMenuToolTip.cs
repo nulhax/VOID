0b7c08bc79912cbd841bda85fc11ce736eb5569f
Scott Emery <scott.ipod@gmail.com>
2014-05-15 11:57:42 +1200

    Server menu tool tips / cleaning up code.
    Signed-off-by: Scott Emery <scott.ipod@gmail.com>

---
+++ Unity/Assets/Scripts/Menu/CMenuToolTip.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CMenuToolTip : MonoBehaviour {
+
+	public UILabel Label;
+
+	// Use this for initialization
+	void Start () 
+	{
+		OnHover(false);
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+
+	}
+
+	void OnHover(bool _bool)
+	{
+		Label.enabled = false; 
+
+		if(_bool)
+		{
+			Label.enabled = true;
+		}
+		else
+		{
+			Label.enabled = false;
+		}
+	}
+
+
+}
